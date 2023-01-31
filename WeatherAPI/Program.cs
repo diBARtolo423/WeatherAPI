3963fe7d98dc82ed8529542b5225a497d50fa6dc
Ross DiBartolo <dibartoloross@gmail.com>
2023-01-31 11:52:35 -0500

    Updated

--- WeatherAPI/Program.cs
+++ WeatherAPI/Program.cs
@@ -5,23 +5,39 @@ using System.IO;
 var client = new HttpClient();
 string key = File.ReadAllText("appsettings.json");
 string APIkey = JObject.Parse(key).GetValue("DefaultKey").ToString();
-Console.WriteLine("What Zip Code would you like to get the weather for?");
-var userInput = Console.ReadLine();
-var weatherURL = $"https://api.openweathermap.org/data/2.5/weather?zip={userInput}&appid={APIkey}&units=imperial";
-var weather = client.GetStringAsync(weatherURL).Result;
-var temp = double.Parse(JObject.Parse(weather)["main"]["temp"].ToString());
-var feels_like = double.Parse(JObject.Parse(weather)["main"]["feels_like"].ToString());
-var humidity = double.Parse(JObject.Parse(weather)["main"]["humidity"].ToString());
-var temp_min = double.Parse(JObject.Parse(weather)["main"]["temp_min"].ToString());
-var temp_max = double.Parse(JObject.Parse(weather)["main"]["temp_max"].ToString());
-var pressure = double.Parse(JObject.Parse(weather)["main"]["pressure"].ToString());
+//Console.WriteLine("What Zip Code would you like to get the weather for?");
+//var userInput = Console.ReadLine();
+//var weatherURL = $"https://api.openweathermap.org/data/2.5/weather?zip={userInput}&appid={APIkey}&units=imperial";
+//var weather = client.GetStringAsync(weatherURL).Result;
+//var temp = double.Parse(JObject.Parse(weather)["main"]["temp"].ToString());
+//var feels_like = double.Parse(JObject.Parse(weather)["main"]["feels_like"].ToString());
+//var humidity = double.Parse(JObject.Parse(weather)["main"]["humidity"].ToString());
+//var temp_min = double.Parse(JObject.Parse(weather)["main"]["temp_min"].ToString());
+//var temp_max = double.Parse(JObject.Parse(weather)["main"]["temp_max"].ToString());
+//var pressure = double.Parse(JObject.Parse(weather)["main"]["pressure"].ToString());
 
-Console.WriteLine($"Current temperature: {temp}");
-Console.WriteLine($"Feels Like: {feels_like}");
-Console.WriteLine($"Humidity: {humidity}");
-Console.WriteLine($"Low: {temp_min}");
-Console.WriteLine($"High: {temp_max}");
-Console.WriteLine($"Pressure: {pressure}");
+
+for (int i = 0; i < 5; i++)
+{
+    Console.WriteLine("What Zip Code would you like to get the weather for?");
+    var userInput = Console.ReadLine();
+    var weatherURL = $"https://api.openweathermap.org/data/2.5/weather?zip={userInput}&appid={APIkey}&units=imperial";
+    var weather = client.GetStringAsync(weatherURL).Result;
+    var temp = double.Parse(JObject.Parse(weather)["main"]["temp"].ToString());
+    var feels_like = double.Parse(JObject.Parse(weather)["main"]["feels_like"].ToString());
+    var humidity = double.Parse(JObject.Parse(weather)["main"]["humidity"].ToString());
+    var temp_min = double.Parse(JObject.Parse(weather)["main"]["temp_min"].ToString());
+    var temp_max = double.Parse(JObject.Parse(weather)["main"]["temp_max"].ToString());
+    var pressure = double.Parse(JObject.Parse(weather)["main"]["pressure"].ToString());
+    Console.WriteLine($"Current temperature: {temp}");
+    Console.WriteLine($"Feels Like: {feels_like}");
+    Console.WriteLine($"Humidity: {humidity}");
+    Console.WriteLine($"Low: {temp_min}");
+    Console.WriteLine($"High: {temp_max}");
+    Console.WriteLine($"Pressure: {pressure}");
+    Console.WriteLine("---------------------");
+}
+
 
 
 
